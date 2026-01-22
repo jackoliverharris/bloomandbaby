04b902a6425f9a98e5545e626a5fb3e569a368ad
Jack Oliver Harris <34695368+jackoliverharris@users.noreply.github.com>
2026-01-22 21:18:09 +0000

    Update paragraph

--- components/BloomCard.tsx
+++ components/BloomCard.tsx
@@ -28,10 +28,10 @@ const BloomCard: React.FC = () => {
               <div className="space-y-8 max-w-lg text-stone-200 text-xl md:text-2xl leading-relaxed font-light">
                 <p>
                   Bloom & Baby is a nurturing community for mums and their babies.
+                </p>
                   <p>
                     Designed for early motherhood, when babies are small enough to rest, be carried, or explore close by.
                   </p>
-                </p>
                 <p>
                   Launching spring 2026 just outside of Bath, it’s a calm weekly pause in the early months of motherhood.
                 </p>
